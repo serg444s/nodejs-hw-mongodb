5d8c813c9506c4b7a1f84c14fe9bf67ffe7eece6
serg444s <143083342+serg444s@users.noreply.github.com>
2024-05-30 15:02:54 +0300

    contactSchema

---
+++ src/db/models/contact.js
@@ -0,0 +1,37 @@
+import { Schema } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    avgMark: {
+      type: Number,
+      required: true,
+    },
+    onDuty: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
