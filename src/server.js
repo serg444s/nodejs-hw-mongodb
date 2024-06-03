0a926edcf707607b8bb5ca908dc14ca6b9651a9c
serg444s <143083342+serg444s@users.noreply.github.com>
2024-06-03 11:26:10 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -49,6 +49,12 @@ export const setupServer = () => {
 
     const contact = await getContactById(contactId);
 
+    if (!contact) {
+      return res.status(404).json({
+        status: 404,
+        message: `Contact with id ${contactId} not found`,
+      });
+    }
     res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
