e00cd852179e98dc1a88998649b7258b12e437c1
serg444s <143083342+serg444s@users.noreply.github.com>
2024-06-03 21:37:56 +0300

    http-errors

---
+++ src/controllers/contacts.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import createHttpError from 'http-errors';
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getAllContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    return res.status(404).json({
+      status: 404,
+      message: `Contact with id ${contactId} not found`,
+    });
+  }
+
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    next(createHttpError(404, `Contact with id ${contactId} not found`));
+    return;
+    // return res.status(404).json({
+    //   status: 404,
+    //   message: `Contact with id ${contactId} not found`,
+    // });
+  }
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
---
+++ src/routers/contacts.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import {
+  getAllContactsController,
+  getContactByIdController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getAllContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+export default router;
