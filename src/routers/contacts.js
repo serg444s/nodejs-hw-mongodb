304e2f013ef0021a584be57e91dc79fd73f2016e
serg444s <143083342+serg444s@users.noreply.github.com>
2024-06-24 17:10:12 +0300

    upd contacts router

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -15,22 +15,22 @@ import {
 
 const router = Router();
 
-router.get('/contacts', ctrlWrapper(getAllContactsController));
+router.get('/', ctrlWrapper(getAllContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/:contactId', ctrlWrapper(getContactByIdController));
 
 router.post(
-  '/contacts',
+  '/',
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
 
 router.patch(
-  '/contacts/:contactId',
+  '/:contactId',
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
--- src/routers/index.js
+++ src/routers/index.js
@@ -4,7 +4,7 @@ import authRouter from './auth.js';
 
 const router = Router();
 
-router.use('/students', contactsRouter);
+router.use('/contacts', contactsRouter);
 router.use('/auth', authRouter);
 
 export default router;
