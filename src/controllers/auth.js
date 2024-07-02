aff4d22bc1e5da86da1387489e2f316667a9c43e
serg444s <143083342+serg444s@users.noreply.github.com>
2024-07-02 16:39:13 +0300

    upd

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -78,15 +78,6 @@ export const refreshUserSessionController = async (req, res) => {
   });
 };
 
-// export const requestResetEmailController = async (req, res) => {
-//   await requestResetToken(req.body.email);
-//   res.json({
-//     message: 'Reset password email was successfully sent!',
-//     status: 200,
-//     data: {},
-//   });
-// };
-
 export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.json({
@@ -95,15 +86,6 @@ export const requestResetEmailController = async (req, res) => {
     data: {},
   });
 };
-
-// export const resetPasswordController = async (req, res) => {
-//   await resetPassword(req.body);
-//   res.json({
-//     message: 'Password was successfully reset!',
-//     status: 200,
-//     data: {},
-//   });
-// };
 
 export const resetPasswordController = async (req, res) => {
   await resetPassword(req.body);
